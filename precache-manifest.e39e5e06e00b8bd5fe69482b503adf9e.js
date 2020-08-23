89af40b0528b9f12273a33c0cd6636e31843a226
josezanle <63666176+josezanle@users.noreply.github.com>
2020-08-23 02:26:04 -0300

    tarea modulo 11

---
+++ precache-manifest.e39e5e06e00b8bd5fe69482b503adf9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c493f5a3284c84f2109ff59642d3ea6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481c84a5df98696940e6",
+    "url": "/static/js/2.233c6fbb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.233c6fbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c198d243939570a1d2",
+    "url": "/static/js/main.e7e65470.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
